e97e818f7ab0e1f44fdfef7495bbc09c0f3e9149
Nick Klein <nklein1@gmail.com>
2020-11-05 11:46:07 -0500

    Updated npm packages and minor housecleaning

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# [Times Table Quiz](http://www.learntomultiply.com)
+# [Times Table Quiz](https://learntomultiply.com)
 
 
 A simple multiplication table quiz built using Gatsby and React. 
-See this repo in action at [learntomultiply.com](http://www.learntomultiply.com)
+See this repo in action at [learntomultiply.com](https://learntomultiply.com)
 
 ## Features:
 - Allows user to "resize" the multiplication table to a desired dimension _(Default is 12x12)_
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Learn To Multiply`,
     description: `Learn to multiply with this simple multiplication table quiz.`,
     author: `Nick Klein`,
-    siteUrl: `http://www.learntomultiply.com`
+    siteUrl: `https://learntomultiply.com`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -23,6 +23,14 @@ module.exports = {
         // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+
+    {
+      resolve: `gatsby-plugin-react-helmet-canonical-urls`,
+      options: {
+        siteUrl: `https://learntomultiply.com`,
+        noTrailingSlash: false
+      },
+    },
     // {
     //   resolve: `gatsby-source-filesystem`,
     //   options: {
@@ -33,12 +41,14 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: `UA-38789595-3`,
+        siteSpeedSampleRate: 100,
+        head: true
       },
     },
     {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
       options: {
-        devMode: true,
+        devMode: false,
       },
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
